9e5444d0e97895b40b4d0d35f72ac481542dfc82
김보경 <rlaqhrud6263@naver.com>
2024-04-15 03:18:06 +0900

    feat: prettierrc.js설정 및 종속성 패키지 설치

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: "always", // 화살표 함수 괄호 사용 방식 Example: (x) => x
+  bracketSpacing: true, // 객체 리터럴에서 괄호에 공백 삽입 여부
+  endOfLine: "lf", // EoF 방식, OS별로 처리 방식이 다름 Linux and macOS
+  jsxBracketSameLine: false, // JSX의 마지막 `>`를 다음 줄로 내릴지 여부
+  jsxSingleQuote: false, // JSX에 singe 쿼테이션 사용 여부
+  quoteProps: "as-needed", // 객체 속성에 쿼테이션 적용 방식
+  semi: true, // 세미콜론 사용 여부
+  singleQuote: false, // single 쿼테이션 사용 여부
+  tabWidth: 2, // 탭 너비
+  printWidth: 120, //  줄 바꿈 할 폭 길이
+  trailingComma: "all", // 여러 줄을 사용할 때, 후행 콤마 사용 방식
+};

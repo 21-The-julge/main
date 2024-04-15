7f962bcbb9ff004194769cbbf89eaf3fd7953fcf
minbo <smb5174@naver.com>
2024-04-15 23:08:49 +0900

    feat: NoticeMessage 구현

---
+++ src/shared/components/Post/NoticeMessage/NoticeMessage.tsx
@@ -0,0 +1,19 @@
+import classNames from "classnames/bind";
+import styles from "@/shared/components/Post/NoticeMessage/NoticeMessage.module.scss";
+
+const cn = classNames.bind(styles);
+
+type NoticeMessageProps = {
+  isPast: boolean;
+  closed: boolean;
+};
+
+export default function NoticeMessage({ isPast, closed }: NoticeMessageProps) {
+  return (
+    <>
+      {closed && isPast && <div className={cn("imgOverlay")}>마감 완료</div>}
+      {isPast && !closed && <div className={cn("imgOverlay")}>지난 공고</div>}
+      {closed && !isPast && <div className={cn("imgOverlay")}>마감 완료</div>}
+    </>
+  );
+}

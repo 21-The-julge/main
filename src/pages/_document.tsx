84ff860d9297c01e90896736c389ce0cde243396
김보경 <rlaqhrud6263@naver.com>
2024-04-15 09:09:19 +0900

    feat: page 일부 생성

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,14 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head />
       <body>
         <Main />
         <NextScript />
-        <div></div>
-        <div></div>
-        <div></div>
       </body>
     </Html>
   );

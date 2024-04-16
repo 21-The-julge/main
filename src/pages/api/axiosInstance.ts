221b3362624020653c6a2c00dbcd59676490de9d
minbo <smb5174@naver.com>
2024-04-16 17:37:52 +0900

    refactor: 변수명 변경

--- src/pages/api/axiosInstance.ts
+++ src/pages/api/axiosInstance.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosInstance } from "axios";
 
-const codeitApiInstance: AxiosInstance = axios.create({
+const axiosInstance: AxiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
   headers: {
     "Content-type": "application/json",
   },
 });
 
-export default codeitApiInstance;
+export default axiosInstance;

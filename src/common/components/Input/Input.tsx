851ec96bd277dc85053d7570dcbddb4bd5f81f1e
김보경 <rlaqhrud6263@naver.com>
2024-04-16 18:53:28 +0900

    feat: type지정

--- src/common/components/Input/Input.tsx
+++ src/common/components/Input/Input.tsx
@@ -1,5 +1,20 @@
+import classNames from "classnames/bind";
 import styles from "./Input.module.scss";
 
-export default function Input() {
-  return <input className={styles.default} placeholder="인풋입니다" />;
+const cn = classNames.bind(styles);
+
+interface InputProps {
+  type: "text" | "email" | "password";
+  size: "sm" | "md" | "lg" | "xl" | "full";
+  placeholder?: string;
 }
+
+export default function Input({ type, size, placeholder }: InputProps) {
+  const className: string = cn("default", size);
+
+  return <input className={className} type={type} placeholder={placeholder} />;
+}
+// component 정의 후 defaultProps 설정
+Input.defaultProps = {
+  placeholder: "",
+};
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,7 +3,7 @@ import Input from "@/common/components/Input/Input";
 export default function LoginPage() {
   return (
     <div>
-      <Input />
+      <Input type="text" size="md" placeholder="김보경" />
     </div>
   );
 }

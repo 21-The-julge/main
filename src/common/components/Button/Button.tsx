7007b85e11eb0398dca3d21bc41cf954e04f280a
suzinxix <suzinxix@gmail.com>
2024-04-16 18:55:25 +0900

    feat: 버튼 컴포넌트 구현

---
+++ src/common/components/Button/Button.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from "react";
+import classNames from "classnames/bind";
+import styles from "./Button.module.scss";
+
+const cn = classNames.bind(styles);
+
+interface Props {
+  children: ReactNode;
+  type: "submit" | "button" | "reset";
+  size: "large" | "medium" | "small";
+  variant?: "solid" | "outline";
+  color?: "primary" | "secondary";
+  disabled?: boolean;
+  onClick: () => void;
+}
+
+export default function Button({ children, type, size, variant, color, disabled, onClick }: Props) {
+  return (
+    <button
+      // eslint-disable-next-line react/button-has-type
+      type={type}
+      disabled={disabled}
+      className={cn("button", size, variant, color)}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = { variant: "solid", color: "primary", disabled: false } as Partial<Props>;

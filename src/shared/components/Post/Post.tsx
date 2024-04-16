d2e58bf3f6feab227b1974be06e7a4051a79e614
minbo <smb5174@naver.com>
2024-04-16 19:47:26 +0900

    feat: Post 컴포넌트 구현

--- src/shared/components/Post/Post.tsx
+++ src/shared/components/Post/Post.tsx
@@ -1,21 +1,20 @@
 import Image from "next/image";
 import classNames from "classnames/bind";
-import styles from "@/common/components/Post/Post.module.scss";
+import styles from "@/shared/components/Post/Post.module.scss";
+import getFullDate from "@/shared/utils/getDate";
 import Clock from "@/images/ic_clock.svg";
 import Location from "@/images/ic_location.svg";
+import NoticeMessage from "./NoticeMessage/NoticeMessage";
+import HighPriceRateBadge from "./HighPriceRateBadge/HighPriceRateBadge";
 
 const cn = classNames.bind(styles);
 
 type Props = {
   imageUrl: string;
-  noticeId: string;
   startsAt: string;
   workhour: number;
   hourlyPay: number;
-  closed?: boolean;
-  isPast?: boolean;
-  noticeShopId?: string | undefined;
-  myShopId?: string;
+  closed: boolean;
   name: string;
   address1: string;
   originalHourlyPay: number;
@@ -23,13 +22,10 @@ type Props = {
 
 export default function Post({
   imageUrl,
-  noticeId,
   startsAt,
   workhour,
   hourlyPay,
   closed,
-  noticeShopId,
-  myShopId,
   name,
   address1,
   originalHourlyPay,
@@ -37,29 +33,41 @@ export default function Post({
   const currentDate = new Date();
   const endDate = new Date(startsAt);
   const isPast = currentDate > endDate;
-  console.log(currentDate, endDate);
-  console.log(isPast);
+
   return (
-    <div className={cn("postContainer")}>
+    <div className={cn("postContainer", { closed, isPast })}>
       <div className={styles.imgContainer}>
-        {isPast && <div className={cn("imgOverlay")}>지난 공1고</div>}
-        <Image src={imageUrl} alt="식당 공고" fill />
+        <NoticeMessage isPast={isPast} closed={closed} />
+        <Image style={{ objectFit: "cover" }} src={imageUrl} alt="식당 공고" fill />
       </div>
       <div className={styles.contentContainer}>
         <div className={styles.restaurantContent}>
           <p className={styles.restaurantName}>{name}</p>
           <div className={styles.clockContainer}>
-            <Clock />
-            <p>시간</p>
+            {closed || isPast ? (
+              <Clock className={styles.clock} fill="#cbc9cf" />
+            ) : (
+              <Clock className={styles.clock} fill="orange" />
+            )}
+            <p>{getFullDate(endDate, workhour)}</p>
           </div>
           <div className={styles.locationContainer}>
-            <Location />
+            {closed || isPast ? (
+              <Location className={styles.location} fill="#cbc9cf" />
+            ) : (
+              <Location className={styles.location} fill="orange" />
+            )}
             {address1}
           </div>
         </div>
         <div className={styles.priceContent}>
-          <p>{hourlyPay}원</p>
-          <div>기존 시급보다 100% 더</div>
+          <p>{hourlyPay?.toLocaleString()}원</p>
+          <HighPriceRateBadge
+            closed={closed}
+            isPast={isPast}
+            hourlyPay={hourlyPay}
+            originalHourlyPay={originalHourlyPay}
+          />
         </div>
       </div>
     </div>

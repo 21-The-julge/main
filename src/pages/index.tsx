0ddf2bf0bf0b3003c65563aeed4bb6addd0d25f3
minbo <smb5174@naver.com>
2024-04-16 20:53:51 +0900

    feat: 나중에 삭제할 코드

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,62 @@
+import Post from "@/shared/components/Post/Post";
+import { useEffect, useState } from "react";
+import styles from "@/pages/index.module.scss";
+import instance from "./api/axiosInstance";
+
+type Res = {
+  item: {
+    id: string;
+    hourlyPay: number;
+    startsAt: string;
+    workhour: number;
+    description: string;
+    closed: boolean;
+    shop: {
+      item: {
+        id: string;
+        name: string;
+        category: string;
+        address1: string;
+        address2: string;
+        description: string;
+        imageUrl: string;
+        originalHourlyPay: number;
+      };
+    };
+  };
+};
+
+type Props = {
+  item: Res[];
+};
+
 export default function Home() {
-  return <div>메인페이지</div>;
+  const [userData, setUserData] = useState<Props["item"]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await instance.get(`notices`);
+      setUserData(response.data.items);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {userData.map((data: Res) => (
+        <div className={styles.a} key={data.item.id}>
+          <Post
+            imageUrl={data.item.shop?.item?.imageUrl}
+            key={data.item.id}
+            startsAt={data.item.startsAt}
+            workhour={data.item.workhour}
+            hourlyPay={data.item.hourlyPay}
+            closed={data.item.closed}
+            name={data.item.shop?.item?.name}
+            address1={data.item.shop?.item?.address1}
+            originalHourlyPay={data.item.shop?.item?.originalHourlyPay}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }

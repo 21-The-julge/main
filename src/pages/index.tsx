58bcb028c5cbf0c813dda5f77f636218c50f8eee
minbo <smb5174@naver.com>
2024-04-17 21:03:11 +0900

    feat: 사용 예시

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import ToastText from "@/common/components/Toast/constants/ToastText";
+import setToast from "@/common/components/Toast/utils/setToast";
+import React from "react";
+
 export default function Home() {
-  return <div>메인페이지</div>;
+  return (
+    <div>
+      <button type="button" onClick={() => setToast(ToastText.success, "ㅇㅇㅇㅇ!")}>
+        success
+      </button>
+      <hr />
+      <button type="button" onClick={() => setToast(ToastText.error, "ㅇㅇㅇㅇ!")}>
+        error
+      </button>
+      <hr />
+
+      <button type="button" onClick={() => setToast(ToastText.info, "ㅇㅇㅇㅇ!")}>
+        info
+      </button>
+      <hr />
+
+      <button type="button" onClick={() => setToast(ToastText.warn, "ㅇㅇㅇㅇ!")}>
+        warn
+      </button>
+    </div>
+  );
 }

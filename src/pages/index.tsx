c2ab3bae7b8e979f146e4b2abb29b128d5d56bbe
minbo <smb5174@naver.com>
2024-04-17 16:23:02 +0900

    refactor: 사용 예시

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,12 @@ export default function Home() {
   const handleClickConfirm = () => {
     setIsModalOpen((prev) => !prev);
   };
+  const handleleftButtononClick = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+  const handlerightButtononClick = () => {
+    setIsModalOpen((prev) => !prev);
+  };
 
   return (
     <>
@@ -17,6 +23,15 @@ export default function Home() {
       {isModalOpen && (
         <div>
           <Modal>
+            <Modal.YesNo
+              message="신청을 취소하시겠어요?"
+              className={styles.test}
+              leftButtononClick={handleleftButtononClick}
+              rightButtononClick={handlerightButtononClick}
+              leftButtonText="아니오"
+              rightButtonText="취소"
+            />
+            <Modal.Confirm message="등록이 완료되었습니다." className={styles.test} onClick={handleClickConfirm} />
             <Modal.Warn
               message="내 프로필을 먼저 등록해 주세요."
               onClick={handleClickConfirm}

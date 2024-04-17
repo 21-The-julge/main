b328d9289587cd1a6bbc7158e76888e26af99bd1
minbo <smb5174@naver.com>
2024-04-17 16:07:53 +0900

    feat: 나중에 merge할 때 삭제예정(사용 예시)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import Modal from "@/common/components/Modal/Modal";
+import styles from "@/pages/index.module.scss";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>메인페이지</div>;
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const handleClickConfirm = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleClickConfirm}>
+        모달
+      </button>
+      {isModalOpen && (
+        <div>
+          <Modal>
+            <Modal.Warn
+              message="내 프로필을 먼저 등록해 주세요."
+              onClick={handleClickConfirm}
+              className={styles.test}
+            />
+          </Modal>
+        </div>
+      )}
+    </>
+  );
 }

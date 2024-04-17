d31ac5cfcd3614c3159febc4987ec07a8c27b3e5
김보경 <rlaqhrud6263@naver.com>
2024-04-18 00:20:17 +0900

    feat: input 기본 style 생성

--- src/common/components/InputField/InputField.tsx
+++ src/common/components/InputField/InputField.tsx
@@ -1,24 +1,31 @@
-import Input from "../Input/Input";
+import classNames from "classnames/bind";
 import styles from "./InputField.module.scss";
+import Input from "../Input/Input";
+
+const cn = classNames.bind(styles);
 
 interface InputFieldProps {
   name: string;
   type?: "text" | "email" | "password";
   placeholder?: string;
   size?: "sm" | "md" | "full";
+  color?: "white" | "gray";
 }
 
-export default function InputField({ name, type = "text", placeholder, size }: InputFieldProps) {
+export default function InputField({ name, type, placeholder, size, color }: InputFieldProps) {
+  const className: string = cn("default", size, color);
+  const errorMessage: string = cn("errorMessage", "red");
+
   return (
-    <>
+    <div className={styles.inputField}>
       <div className={styles.label}>{name}</div>
       {/* <SuffixIcon /> */}
-      <div className={styles.inputField}>
+      <div className={className}>
         <Input name={name} type={type} placeholder={placeholder} size={size} />
       </div>
       {/* <PreffixIcon /> */}
-      {/* <span className={styles.errorMessage}>{"잘못된 이메일 입니다."}</span> */}
-    </>
+      <span className={errorMessage}>잘못된 이메일 입니다.</span>
+    </div>
   );
 }
 
@@ -27,4 +34,5 @@ InputField.defaultProps = {
   placeholder: "",
   size: "md",
   type: "text",
+  color: "white",
 };

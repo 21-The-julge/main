2941540f0866fcb8f867465709ff1a29999d8ca0
lim sangmin <sangmin0205@gmail.com>
2024-04-17 14:11:33 +0900

    feat: Badge 컴포넌트 제작

--- src/common/components/Badge/Badge.tsx
+++ src/common/components/Badge/Badge.tsx
@@ -1,24 +1,38 @@
+import React from "react";
 import CloseIcon from "@/images/ic_close.svg";
 import styles from "./badge.module.scss";
 import classNames from "classnames";
 
 interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {
-  color?: string;
+  color?: "blue" | "green" | "red";
+  mobile?: boolean;
   close?: boolean;
   onClick?: React.MouseEventHandler<HTMLDivElement>;
+  className?: string; // 사용자가 추가할 수 있는 클래스 이름
 }
 
-export default function Badge({ color, close = false, children, onClick, ...props }: BadgeProps) {
+export default function Badge({
+  color,
+  close = false,
+  mobile = true,
+  children,
+  onClick,
+  className,
+  ...props
+}: BadgeProps) {
   const cn = classNames.bind(styles);
+  // 기본 "badge" 클래스와 조건부 색상 클래스, 사용자 정의 클래스를 결합
+  const badgeClasses = cn([styles.badge], [styles.className], {
+    [styles.mobile]: mobile,
+    [styles.blue]: color === "blue",
+    [styles.green]: color === "green",
+    [styles.red]: color === "red",
+  });
 
-  return close ? (
-    <div className={cn("badge")} {...props}>
+  return (
+    <div className={badgeClasses} {...props}>
       {children}
-      <CloseIcon width="16" height="16" fill="#EA3C12" onClick={onClick} />
-    </div>
-  ) : (
-    <div className={cn("badge")} onClick={onClick} {...props}>
-      {children}
+      {close && <CloseIcon width="16" height="16" fill="#EA3C12" onClick={onClick} />}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,3 @@
-import Badge from "@/common/components/Badge/Badge";
-
 export default function Home() {
-  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    console.log("Badge Clicked!");
-  };
-  return (
-    <>
-      <Badge onClick={handleClick}>안녕</Badge>
-      <Badge close={true} onClick={handleClick}>
-        하세요
-      </Badge>
-      ;
-    </>
-  );
+  return <div>메인페이지</div>;
 }
-//<div>메인페이지</div>;

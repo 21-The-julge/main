1f2ee64d48cd2a3910447230ae6204f6080e3592
lim sangmin <sangmin0205@gmail.com>
2024-04-18 15:53:04 +0900

    refactor: 코드 리뷰 수정 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,3 @@
-import Badge from "@/shared/components/Badge/Badge";
-
 export default function Home() {
-  return (
-    <>
-      <Badge color="red" hasCloseIcon={true}>
-        서울
-      </Badge>
-      <Badge color="red" hasCloseIcon={true}>
-        서울시
-      </Badge>
-      <Badge color="blue" styleMode="responsive">
-        승인 완료
-      </Badge>
-    </>
-  );
+  return <div>메인페이지</div>;
 }

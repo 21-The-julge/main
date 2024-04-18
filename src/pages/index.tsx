9412420a8661e9b86108178a9b630078db389d43
minbo <smb5174@naver.com>
2024-04-18 19:53:23 +0900

    refactor: 스켈레톤 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Post } from "@/shared/components/index";
 import { useEffect, useState } from "react";
 import styles from "@/pages/index.module.scss";
+import PostSkeleton from "@/shared/components/Post/Skeleton/PostSkeleton";
 import instance from "./api/axiosInstance";
 
 type Res = {
@@ -28,31 +29,44 @@ type Res = {
 
 export default function Home() {
   const [userData, setUserData] = useState<Res[]>([]);
+  const [isloading, setIsloading] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
+      setIsloading(false);
       const response = await instance.get("notices");
       setUserData(response.data.items);
+      setIsloading(true);
     };
     fetchData();
   }, []);
 
   return (
     <div>
-      {userData.map((data: Res) => (
-        <div className={styles.a} key={data.item.id}>
-          <Post
-            imageUrl={data.item.shop?.item?.imageUrl}
-            key={data.item.id}
-            startsAt={data.item.startsAt}
-            workhour={data.item.workhour}
-            hourlyPay={data.item.hourlyPay}
-            closed={data.item.closed}
-            name={data.item.shop?.item?.name}
-            address={data.item.shop?.item?.address1}
-            originalHourlyPay={data.item.shop?.item?.originalHourlyPay}
-          />
+      {/* {isloading ? (
+        userData.map((data: Res) => (
+          <div className={styles.a} key={data.item.id}>
+            <Post
+              imageUrl={data.item.shop?.item?.imageUrl}
+              key={data.item.id}
+              startsAt={data.item.startsAt}
+              workhour={data.item.workhour}
+              hourlyPay={data.item.hourlyPay}
+              closed={data.item.closed}
+              name={data.item.shop?.item?.name}
+              address={data.item.shop?.item?.address1}
+              originalHourlyPay={data.item.shop?.item?.originalHourlyPay}
+            />
+          </div>
+        ))
+      ) : (
+        <div className={styles.a}>
+          <PostSkeleton />
         </div>
-      ))}
+      )} */}
+      <div className={styles.a}>
+        <PostSkeleton />
+      </div>
     </div>
   );
 }

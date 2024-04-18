a2fba798f581234185334c5f77fb4332436a925c
KimChangmin <zzangmin3@gmail.com>
2024-04-18 20:31:13 +0900

    feat: index.ts 파일 선언

---
+++ src/pages/api/api-hooks/index.ts
@@ -0,0 +1,8 @@
+export * from "./alerts";
+export * from "./applications";
+export * from "./notices";
+export * from "./users";
+export * from "./shops";
+
+export { default as PostSignIn } from "./authentications";
+export { default as PostPresignedURL } from "./images";

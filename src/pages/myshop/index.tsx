6eb180b2854a3c745e7ca920a915c6116f6acb5d
minbo <smb5174@naver.com>
2024-04-18 22:19:57 +0900

    feat: 가게 정보 상세 페이지 제작

---
+++ src/pages/myshop/index.tsx
@@ -0,0 +1,15 @@
+import ShopRegister from "@/page-layout/MyShopLayout/component/ShopRegister/ShopRegister";
+import styles from "@/pages/myshop/myshop.module.scss";
+import classNames from "classnames/bind";
+
+const cn = classNames.bind(styles);
+
+export default function MyShop() {
+  return (
+    <div className={cn("container")}>
+      <div style={{ height: 70, backgroundColor: "red" }} />
+      <ShopRegister />
+      <div style={{ height: 70, backgroundColor: "red" }} />
+    </div>
+  );
+}

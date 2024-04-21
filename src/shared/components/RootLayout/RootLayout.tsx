b9dab5b3550de1e55758fb5ad79a4ee2cdd26461
KimChangmin <zzangmin3@gmail.com>
2024-04-21 14:17:17 +0900

    폴더 명 PascalCase로 수정

--- src/shared/components/RootLayout/RootLayout.tsx
+++ src/shared/components/RootLayout/RootLayout.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from "react";
+import Gnb from "@/common/components/Gnb/Gnb";
+import Footer from "@/common/components/Footer/Footer";
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -7,7 +9,9 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <div>
+      <Gnb />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 }

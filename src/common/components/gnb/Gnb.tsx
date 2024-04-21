79b0676d275c1c7fd854e5785e141dffaba238fb
KimChangmin <zzangmin3@gmail.com>
2024-04-21 14:15:12 +0900

    temp

--- src/common/components/gnb/Gnb.tsx
+++ src/common/components/gnb/Gnb.tsx
@@ -3,22 +3,18 @@ import Link from "next/link";
 import LOGO from "@/images/logo.svg";
 import styles from "./Gnb.module.scss";
 
-import UserAction from "./gnb-components/UserAction";
+import UserAction from "./GnbComponents/UserAction";
 
 const cn = classNames.bind(styles);
-
-const Logo = (
-  <Link href="/">
-    <LOGO className={cn("logo")} />
-  </Link>
-);
 
 const Input = <input className={cn("input")} type="text" placeholder="가게 이름으로 찾아보세요." />;
 
 export default function Gnb() {
   return (
     <div className={cn("container")}>
-      {Logo}
+      <Link href="/">
+        <LOGO className={cn("logo")} />
+      </Link>
       {Input}
       <UserAction />
     </div>

c04e258033fa529f771429f87b25e7837c44c461
lim sangmin <sangmin0205@gmail.com>
2024-04-22 19:04:36 +0900

    refactor : pr 반영

--- src/common/components/Table/Table.tsx
+++ src/common/components/Table/Table.tsx
@@ -1,31 +1,16 @@
-import React, { useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./Table.module.scss";
-import Button from "@/common/components/Button/Button";
 
 interface TableProps {
-  columns: Array<{
+  columns: {
     header: string;
     accessor: string;
-  }>;
-  data: Array<any>;
-  position?: "employee" | "employer";
+  }[];
+  data: { [key: string]: any }[];
 }
-
 const cn = classNames.bind(styles);
 
-export default function Table({ columns, data, position }: TableProps) {
-  const [tableData, setTableData] = useState(data);
-
-  // 버튼 클릭 후 발생되는 이벤트 // 함수의 인덱스를 map함수의 index로 넣어야해서 handleClick 함수대신 이렇게 표현했는데 맞는지 살짝 헷갈리네요
-  // {() => handleStatusChange(index, "rejected")}
-  const handleStatusChange = (index: number, buttonStatus: string) => {
-    const newData = [...tableData];
-    newData[index].status = buttonStatus;
-    setTableData(newData);
-    // 여기에 PUT /shops/{shop_id}/notices/{notice_id}/applications/{application_id} 넣으면 될 것 같습니다 아직 커스텀 훅 안만들어져서 일단 구현 안했습니다
-  };
-
+export default function Table({ columns, data }: TableProps) {
   return (
     <table className={cn("table")}>
       <thead>
@@ -36,36 +21,10 @@ export default function Table({ columns, data, position }: TableProps) {
         </tr>
       </thead>
       <tbody>
-        {tableData.map((item, index) => (
-          <tr key={index}>
+        {data.map((item, index) => (
+          <tr key={item.id || index}>
             {columns.map((column) => (
-              <td key={`${index}-${column.accessor}`}>
-                {column.accessor === "status" && position === "employer" && item.status === "pending" ? (
-                  // column.accessor === "status" 조건부를 달아둔 이유는 status가 없는 테이블이 있을수 있고 이를 반영해야 공용 컴포넌트 역할을 한다 생각해서 넣었습니다.
-                  <div className={cn("tableButton")}>
-                    <Button
-                      type="button"
-                      size="small"
-                      variant="outline"
-                      color="primary"
-                      onClick={() => handleStatusChange(index, "rejected")}
-                    >
-                      거절하기
-                    </Button>
-                    <Button
-                      type="button"
-                      size="small"
-                      variant="outline"
-                      color="secondary"
-                      onClick={() => handleStatusChange(index, "approved")}
-                    >
-                      승인하기
-                    </Button>
-                  </div>
-                ) : (
-                  item[column.accessor]
-                )}
-              </td>
+              <td key={`${item.id || index}-${column.accessor}`}>{item[column.accessor]}</td>
             ))}
           </tr>
         ))}

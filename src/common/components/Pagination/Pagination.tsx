aa160e8b9e28082d36a5385373b24b9955b706de
lim sangmin <sangmin0205@gmail.com>
2024-04-24 14:23:21 +0900

    refactor: pr반영

--- src/common/components/Pagination/Pagination.tsx
+++ src/common/components/Pagination/Pagination.tsx
@@ -7,17 +7,17 @@ import styles from "./Pagination.module.scss";
 
 interface PaginationProps {
   currentPage: number;
-  totalPages: number;
-  onPageChange: (page: number) => void;
+  totalPage: number;
+  onPageClick: (page: number) => void;
   pageNumberLimit?: number;
 }
 
 const cn = classNames.bind(styles);
 
-export default function Pagination({ currentPage, totalPages, onPageChange, pageNumberLimit = 7 }: PaginationProps) {
+export default function Pagination({ currentPage, totalPage, onPageClick, pageNumberLimit = 7 }: PaginationProps) {
   const currentPageGroup = Math.ceil(currentPage / pageNumberLimit); // 현재 페이지가 속한 페이지 그룹 Ex) (1~7)1그룹 (8~14)2그룹
   const currentGroupFirstIndex = (currentPageGroup - 1) * pageNumberLimit + 1;
-  const currentGroupLastIndex = Math.min(currentGroupFirstIndex + pageNumberLimit - 1, totalPages);
+  const currentGroupLastIndex = Math.min(currentGroupFirstIndex + pageNumberLimit - 1, totalPage);
 
   // 현재 페이지 그룹에 속한 페이지 번호 배열 생성
   const pages = Array.from(
@@ -25,23 +25,23 @@ export default function Pagination({ currentPage, totalPages, onPageChange, page
     (_, i) => currentGroupFirstIndex + i,
   );
 
-  const moveNextGroup = () => {
-    const nextGroupPage = currentPageGroup * pageNumberLimit + 1;
-    if (nextGroupPage <= totalPages) {
-      onPageChange(nextGroupPage);
-    }
-  };
-
   const movePrevGroup = () => {
     const prevGroupPage = (currentPageGroup - 2) * pageNumberLimit + 1;
     if (prevGroupPage > 0) {
-      onPageChange(prevGroupPage);
+      onPageClick(prevGroupPage);
+    }
+  };
+
+  const moveNextGroup = () => {
+    const nextGroupPage = currentPageGroup * pageNumberLimit + 1;
+    if (nextGroupPage <= totalPage) {
+      onPageClick(nextGroupPage);
     }
   };
 
   return (
     <div className={cn("pagination")}>
-      <button type="button" aria-label="처음 페이지로" onClick={() => onPageChange(1)}>
+      <button type="button" aria-label="처음 페이지로" onClick={() => onPageClick(1)}>
         <LeftArrowDoubleIcon className={cn("buttonArrow")} />
       </button>
       {currentPageGroup > 0 && (
@@ -56,7 +56,7 @@ export default function Pagination({ currentPage, totalPages, onPageChange, page
             className={cn("buttonPage", { active: currentPage === page })}
             key={page}
             aria-label={`페이지 ${page}로 이동`}
-            onClick={() => onPageChange(page)}
+            onClick={() => onPageClick(page)}
           >
             {page}
           </button>
@@ -65,7 +65,7 @@ export default function Pagination({ currentPage, totalPages, onPageChange, page
       <button type="button" aria-label="다음 페이지 그룹" onClick={moveNextGroup}>
         <RightArrowIcon className={cn("buttonArrow")} />
       </button>
-      <button type="button" aria-label="마지막 페이지로" onClick={() => onPageChange(totalPages)}>
+      <button type="button" aria-label="마지막 페이지로" onClick={() => onPageClick(totalPage)}>
         <RightArrowDoubleIcon className={cn("buttonArrow")} />
       </button>
     </div>

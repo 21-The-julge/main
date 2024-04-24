6000ee555e28013f2842bdd67a83460fb3f4b036
김보경 <rlaqhrud6263@naver.com>
2024-04-24 14:43:07 +0900

    style: login page - index 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,5 @@
 import { InputField } from "@/common/components";
 import Dropdown from "@/common/components/FieldGroup/Dropdown/Dropdown";
-import FieldGroup from "@/common/components/FieldGroup/FieldGroup";
 import { BUSINESS_OPTIONS } from "@/common/constants";
 
 export default function LoginPage() {
@@ -12,10 +11,6 @@ export default function LoginPage() {
     <>
       <InputField unit="시급" size="md" className="30rem" />
       <Dropdown name="123" options={BUSINESS_OPTIONS} placeholder="입력" className="30rem" />
-
-      <FieldGroup field="input" type="search" label="김보경" name="김보경" unit="원" />
-      <FieldGroup field="dropdown" label="김가은" name="김가은" />
-      <FieldGroup label="김보경" name="김보경" required field="textarea" placeholder="입력" />
     </>
   );
 }

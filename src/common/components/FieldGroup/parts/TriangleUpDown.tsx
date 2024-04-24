064778d45c4beeb9ccf04420c8a4f0052718d158
김보경 <rlaqhrud6263@naver.com>
2024-04-24 18:40:02 +0900

    style: triangle up down svg파일 알맞게 수정

--- src/common/components/FieldGroup/parts/TriangleUpDown.tsx
+++ src/common/components/FieldGroup/parts/TriangleUpDown.tsx
@@ -1,4 +1,4 @@
-import TriangleUp from "@/images/ic_triangleUp.svg";
+// import TriangleUp from "@/images/ic_triangleUp.svg";
 import TriangleDown from "@/images/ic_triangleDown.svg";
 
 import classNames from "classnames/bind";
@@ -11,9 +11,12 @@ interface TriangleUpDownProps {
 }
 
 export default function TriangleUpDown({ isOpen }: TriangleUpDownProps) {
+  const className = cn("triangleUpDown", { rotate: isOpen === true });
+
   return (
-    <div className={cn("TriangleUpDown")}>
-      {isOpen ? <TriangleDown width={16} height={16} /> : <TriangleUp width={16} height={16} />}
+    <div className={className}>
+      {/* {isOpen ? <TriangleDown width={16} height={16} /> : <TriangleUp width={16} height={16} />} */}
+      <TriangleDown width={16} height={16} />
     </div>
   );
 }

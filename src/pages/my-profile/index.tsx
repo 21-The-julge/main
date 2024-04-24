3e7c2b9c8b96dad0160ebffb5bc481f48c66a706
minbo <smb5174@naver.com>
2024-04-24 19:02:15 +0900

    feat: 내 프로필 영역 구현

---
+++ src/pages/my-profile/index.tsx
@@ -0,0 +1,74 @@
+import MyProfile from "@/page-layout/MyProfileLayout/MyProfile/MyProfile";
+import { axiosInstance, axiosInstanceToken } from "@/shared/apis/axiosInstance";
+import RootLayout from "@/shared/components/RootLayout/RootLayout";
+import { useEffect } from "react";
+
+interface Shop {
+  name: string;
+}
+
+interface Notice {
+  hourlyPay: number;
+  startsAt: string;
+  workhour: number;
+}
+
+interface Item {
+  item: {
+    shop: {
+      item: Shop;
+    };
+    notice: {
+      item: Notice;
+    };
+  };
+}
+
+interface ApiData {
+  myProfile: {
+    id: string;
+    email: string;
+    type: string;
+    name: string;
+    phone: string;
+    address: string;
+    bio: string;
+  };
+  registeredNotice: {
+    item: Item[];
+  };
+}
+
+export async function getServerSideProps() {
+  let myProfile;
+  let registeredNotice;
+
+  try {
+    const response = await axiosInstance.get("users/af968af9-03b1-448e-b8f3-f3823fc7f6a8");
+    myProfile = response?.data.item ?? [];
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    const response = await axiosInstance.get("users/af968af9-03b1-448e-b8f3-f3823fc7f6a8/applications");
+    registeredNotice = response?.data ?? [];
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {
+    props: {
+      myProfile,
+      registeredNotice,
+    },
+  };
+}
+
+export default function MyShop({ myProfile, registeredNotice }: ApiData) {
+  return (
+    <RootLayout>
+      <MyProfile myProfile={myProfile} />
+    </RootLayout>
+  );
+}
--- src/shared/apis/axiosInstance.ts
+++ src/shared/apis/axiosInstance.ts
@@ -12,9 +12,10 @@ const axiosInstanceToken = axios.create({
   headers: {
     "Content-type": "application/json",
     Authorization:
-      typeof window !== "undefined" && sessionStorage.getItem("token")
-        ? `Bearer ${sessionStorage.getItem("token")}`
-        : "",
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJhZjk2OGFmOS0wM2IxLTQ0OGUtYjhmMy1mMzgyM2ZjN2Y2YTgiLCJpYXQiOjE3MTM5Mjg5MDl9.YcxhVmCY46wK3r7go2gUZDl8pdyfBW6eO3WvBfXYR3k",
+    // typeof window !== "undefined" && sessionStorage.getItem("token")
+    //   ? `Bearer ${sessionStorage.getItem("token")}`
+    //   : "",
   },
 });
 

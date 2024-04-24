d29de2df4cfe38a0d604f6bfce8395e334d42150
lim sangmin <sangmin0205@gmail.com>
2024-04-24 23:39:38 +0900

    feat: noticedetail 수정중

--- src/pages/noticeDetail/index.tsx
+++ src/pages/noticeDetail/index.tsx
@@ -1,12 +1,13 @@
 import NoticeDetailInfo from "@/page-layout/NoticesLayout/components/NoticeDetailInfo/NoticeDetailInfo";
-//import NoticesViewedRecent from "@/page-layout/NoticesLayout/components/NoticesViewedRecent/NoticesViewedRecent";
+import NoticesViewedRecent from "@/page-layout/NoticesLayout/components/NoticesViewedRecent/NoticesViewedRecent";
 import RootLayout from "@/shared/components/RootLayout/RootLayout";
 
 export default function NoticeDetail() {
   return (
     <RootLayout>
       <NoticeDetailInfo />
+      <NoticesViewedRecent />
     </RootLayout>
   );
 }
-//<NoticesViewedRecent />
+//

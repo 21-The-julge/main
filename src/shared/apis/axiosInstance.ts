4b58c4fb51823bf00e6262d63c3604dd772b1b11
minbo <smb5174@naver.com>
2024-04-26 14:19:35 +0900

    refactor: 주석 제거

--- src/shared/apis/axiosInstance.ts
+++ src/shared/apis/axiosInstance.ts
@@ -12,7 +12,6 @@ const axiosInstanceToken = axios.create({
   headers: {
     "Content-type": "application/json",
     Authorization:
-      // "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJhZjk2OGFmOS0wM2IxLTQ0OGUtYjhmMy1mMzgyM2ZjN2Y2YTgiLCJpYXQiOjE3MTM5Mjg5MDl9.YcxhVmCY46wK3r7go2gUZDl8pdyfBW6eO3WvBfXYR3k",
       typeof window !== "undefined" && sessionStorage.getItem("token")
         ? `Bearer ${sessionStorage.getItem("token")}`
         : "",

58cb0c67e42d09e96130873f2ceb26c84f4122af
김보경 <rlaqhrud6263@naver.com>
2024-04-27 18:55:09 +0900

    refactor: textarea를 FieldGroup에서 분리 하였습니다

--- src/common/components/FieldGroup/Textarea/Textarea.tsx
+++ src/common/components/FieldGroup/Textarea/Textarea.tsx
@@ -1,24 +1,36 @@
+import { TextareaHTMLAttributes } from "react";
+
 import classNames from "classnames/bind";
 import styles from "./Textarea.module.scss";
-import { TextareaProps } from "../type";
+
+import { Label } from "../parts";
 
 const cn = classNames.bind(styles);
 
-export default function Textarea({ name, value, required, placeholder, onTextareaChange, rows }: TextareaProps) {
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  rows?: number;
+  name?: string;
+  value?: string | number;
+  required?: boolean;
+  label?: string;
+  className?: string;
+}
+
+export default function Textarea({ name, value, label, rows = 4, className, ...rest }: TextareaProps) {
   return (
-    <div className={cn("textareaBox")}>
-      <textarea
-        rows={rows}
-        className={cn("textarea")}
-        id={name}
-        name={name}
-        autoCorrect="off"
-        required={required}
-        placeholder={placeholder}
-        onChange={onTextareaChange}
-      >
-        {value}
-      </textarea>
+    <div className={cn("field")}>
+      {label && <Label label={label} htmlFor={name} required={rest.required} />}
+      <div className={cn("textareaBox", className)}>
+        <textarea
+          rows={rows}
+          className={cn("textarea")}
+          id={name}
+          name={name}
+          autoCorrect="off"
+          value={value}
+          {...rest}
+        />
+      </div>
     </div>
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,18 @@
-// import FieldGroup from "@/common/components/FieldGroup/FieldGroup";
-
-import { InputField } from "@/common/components/FieldGroup";
+import { InputField, Textarea } from "@/common/components/FieldGroup";
+import { ChangeEvent } from "react";
 
 export default function LoginPage() {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    console.log(e.target.value);
+  };
+
   return (
     <>
-      <InputField unit="시간" size="md" className="test" placeholder="입력" />
-      {/* <FieldGroup field="dropdown" options={CATEGORIES} /> */}
+      <InputField unit="시간" size="md" className="test" placeholder="입력" onChange={handleChange} />
+      <Textarea placeholder="입력" onChange={handleTextareaChange} label="테스트" name="테스트" required />
     </>
   );
 }

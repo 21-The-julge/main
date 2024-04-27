80b8fe66b852dd7777997cab4d9672f687c0fbcc
minbo <smb5174@naver.com>
2024-04-28 01:40:48 +0900

    refactor: 컴포넌트명 변경

--- src/pages/register-my-shop/index.tsx
+++ src/pages/register-my-shop/index.tsx
@@ -1,10 +1,10 @@
-import RegisterMyShop from "@/page-layout/RegisterMyShopLayout/component/RegisterMyShop/RegisterMyShop";
+import RegisterMyShopLayout from "@/page-layout/RegisterMyShopLayout/component/RegisterMyShop/RegisterMyShop";
 import RootLayout from "@/shared/components/RootLayout/RootLayout";
 
-export default function MyShop() {
+export default function RegisterMyShop() {
   return (
     <RootLayout needFooter={false}>
-      <RegisterMyShop />
+      <RegisterMyShopLayout />
     </RootLayout>
   );
 }

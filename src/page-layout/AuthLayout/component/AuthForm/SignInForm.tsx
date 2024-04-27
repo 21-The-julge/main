474fdc623c1fb7f607170cb084f8f117674e972d
김보경 <rlaqhrud6263@naver.com>
2024-04-27 21:55:35 +0900

    feat: SignInForm에 Modal을 추가했습니다

--- src/page-layout/AuthLayout/component/AuthForm/SignInForm.tsx
+++ src/page-layout/AuthLayout/component/AuthForm/SignInForm.tsx
@@ -1,14 +1,18 @@
-// import { useRouter } from "next/router";
+import classNames from "classnames/bind";
+
+import { useState } from "react";
+import { useRouter } from "next/router";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
-import { ERROR_MESSAGE, PLACEHOLDERS } from "@/common/constants";
+import { ERROR_MESSAGE, PLACEHOLDERS, ROUTE } from "@/common/constants";
 import { Button, InputField } from "@/common/components";
 import { PostSignInParams } from "@/shared/apis/apiType";
-// import { usePostSignIn } from "@/shared/apis/api-hooks";
+import { usePostSignIn } from "@/shared/apis/api-hooks";
 
-import classNames from "classnames/bind";
+import ConfirmModal from "@/common/components/Modal/ConfirmModal/ConfirmModal";
+
 import styles from "./SignInForm.module.scss";
 
 const cn = classNames.bind(styles);
@@ -23,12 +27,15 @@ const EMAIL = "email";
 const PASSWORD = "password";
 
 export default function SignInForm() {
-  // const router = useRouter();
+  const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<PostSignInParams>({
     mode: "onTouched",
     resolver: zodResolver(schema),
@@ -38,44 +45,56 @@ export default function SignInForm() {
     },
   });
 
-  // const { mutate: login } = usePostSignIn();
+  const handleModalButtonClick = () => {
+    setIsModalOpen(false);
+    router.reload();
+    setError(PASSWORD, { message: ERROR_MESSAGE.PASSWORD.INCORRECT });
+  };
+
+  const { mutate: login } = usePostSignIn();
 
   const onSubmit: SubmitHandler<PostSignInParams> = (payload) => {
     // eslint-disable-next-line no-console
     console.log("로그인 payload:", payload);
-    // login(payload, {
-    //   onSuccess: () => {
-    //     console.log("로그인 성공");
-    //   },
-    //   onError: (error) => {
-    //     console.error("로그인 실패", error);
-    //   },
-    // });
+    login(payload, {
+      onSuccess: () => {
+        router.push(ROUTE.NOTICES);
+      },
+      onError: () => {
+        setModalMessage(ERROR_MESSAGE.PASSWORD.INCORRECT);
+        setIsModalOpen(true);
+      },
+    });
   };
 
   return (
-    <form className={cn("formBox")} onSubmit={handleSubmit(onSubmit)}>
-      <InputField
-        {...register(EMAIL)}
-        type={EMAIL}
-        label="이메일"
-        placeholder={PLACEHOLDERS.EMAIL}
-        name={EMAIL}
-        isError={!!errors.email}
-        errorMessage={errors.email?.message}
-      />
-      <InputField
-        {...register(PASSWORD)}
-        type={PASSWORD}
-        label="비밀번호"
-        placeholder={PLACEHOLDERS.PASSWORD}
-        name={PASSWORD}
-        isError={!!errors.password}
-        errorMessage={errors.password?.message}
-      />
-      <Button type="submit" size="large">
-        로그인하기
-      </Button>
-    </form>
+    <>
+      <form className={cn("formBox")} onSubmit={handleSubmit(onSubmit)}>
+        <InputField
+          {...register(EMAIL)}
+          type={EMAIL}
+          label="이메일"
+          placeholder={PLACEHOLDERS.EMAIL}
+          name={EMAIL}
+          isError={!!errors.email}
+          errorMessage={errors.email?.message}
+        />
+        <InputField
+          {...register(PASSWORD)}
+          type={PASSWORD}
+          label="비밀번호"
+          placeholder={PLACEHOLDERS.PASSWORD}
+          name={PASSWORD}
+          isError={!!errors.password}
+          errorMessage={errors.password?.message}
+        />
+        <Button type="submit" size="large">
+          로그인하기
+        </Button>
+      </form>
+      {isModalOpen && (
+        <ConfirmModal className={cn("signInFormModal")} message={modalMessage} onClick={handleModalButtonClick} />
+      )}
+    </>
   );
 }
--- src/shared/apis/api-hooks/useAuthentications.ts
+++ src/shared/apis/api-hooks/useAuthentications.ts
@@ -1,13 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
 import useUserDataStore from "@/shared/hooks/useUserDataStore";
 import { axiosInstance } from "../axiosInstance";
+import { PostSignInParams } from "../apiType";
 // import { PostSignInParams } from "../apiType";
 
 // 로그인 POST 요청
 export default function usePostSignIn() {
   const { setToken, setUserId, setType, setLoggedIn } = useUserDataStore();
   return useMutation({
-    mutationFn: async (payload) => {
+    mutationFn: async (payload: PostSignInParams) => {
       const { data } = await axiosInstance.post("/token", payload);
       return data;
     },

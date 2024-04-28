ffa65b31d8319f346cc852b4f5dcbbec2b70af0f
minbo <smb5174@naver.com>
2024-04-28 13:16:11 +0900

    refactor: token 수정

--- src/shared/apis/api-hooks/useImages.ts
+++ src/shared/apis/api-hooks/useImages.ts
@@ -8,9 +8,7 @@ export default function usePostPresignedURL() {
   const { token } = useUserDataStore();
   const mutation = useMutation({
     mutationFn: async (bodyData: PostPresignedURLParams) => {
-      const { data } = await axiosInstanceToken(
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ZmNkMGJhOC1jZjhmLTRmY2YtOTdiMS04MmY2NTY3ZmU0Y2MiLCJpYXQiOjE3MTMyNDY1NjZ9.eexZyuw_-CoOfbHnPDF9RrAQNq9YWnLEQAesbCOlSo0",
-      ).post("/images", bodyData);
+      const { data } = await axiosInstanceToken(token).post("/images", bodyData);
       return data;
     },
   });

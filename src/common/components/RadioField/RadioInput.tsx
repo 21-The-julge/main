f4598b53a88056bbae68247fca9c25d3ee1a7022
김보경 <rlaqhrud6263@naver.com>
2024-04-29 01:21:39 +0900

    feat: RadioField 완성

--- src/common/components/RadioField/RadioInput.tsx
+++ src/common/components/RadioField/RadioInput.tsx
@@ -2,14 +2,15 @@ import classNames from "classnames/bind";
 import { InputHTMLAttributes, forwardRef } from "react";
 
 import styles from "./RadioInput.module.scss";
+import RadioToggle from "./RadioToggle";
 
 const cn = classNames.bind(styles);
 
 interface RadioInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  id?: string;
-  name?: string; // fieldset안에서 name값이 같아야 하나만 checked가능
-  label?: string;
-  ckecked?: boolean;
+  id: string;
+  name: string; // fieldset안에서 name값이 같아야 하나만 checked가능
+  label: string;
+  checked: boolean;
 }
 
 export default forwardRef<HTMLInputElement, RadioInputProps>(function RadioInput(
@@ -20,6 +21,7 @@ export default forwardRef<HTMLInputElement, RadioInputProps>(function RadioInput
 
   return (
     <label htmlFor={id} className={className}>
+      <RadioToggle isChecked={checked} />
       <input ref={ref} id={id} className={cn("input")} type="radio" name={name} value={value} {...rest} />
       {label}
     </label>

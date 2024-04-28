37c3cc1328b0bc6914cb08c28daf947812a88930
lim sangmin <sangmin0205@gmail.com>
2024-04-29 00:42:27 +0900

    feat: 라우터 재설정

--- src/pages/my-notice-Detail/shops/[shopId]/notices/[noticeId].tsx
+++ src/pages/my-notice-Detail/shops/[shopId]/notices/[noticeId].tsx
@@ -5,15 +5,11 @@ import { useRouter } from "next/router";
 
 export default function NoticeDetail() {
   const router = useRouter();
-  //const { shopId, noticeId } = router.query;
-
-  const shopId = router.query.shopId || "7f79ef71-553d-4c50-a35c-7cb48e978b4c";
-  const noticeId = router.query.noticeId || "71520b38-69f6-4cc6-b8a3-e3ef859ed9a6";
+  const { shopId, noticeId } = router.query;
 
   if (typeof shopId !== "string" || typeof noticeId !== "string") {
     return <div>Loading</div>;
   }
-
   return (
     <RootLayout>
       <NoticeDetailInfo shopId={shopId} noticeId={noticeId} myNotice={false} />

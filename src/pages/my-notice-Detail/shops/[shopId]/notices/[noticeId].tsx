0eeec0e3e233c30c03f8ef83171825a3fd46869c
lim sangmin <sangmin0205@gmail.com>
2024-04-29 00:37:14 +0900

    feat: page 디레토릭 변경

--- src/pages/my-notice-Detail/shops/[shopId]/notices/[noticeId].tsx
+++ src/pages/my-notice-Detail/shops/[shopId]/notices/[noticeId].tsx
@@ -4,11 +4,15 @@ import NoticesRecent from "@/page-layout/NoticesLayout/components/NoticesRecent/
 import { useRouter } from "next/router";
 
 export default function NoticeDetail() {
-  const shopId = "7f79ef71-553d-4c50-a35c-7cb48e978b4c";
-  const noticeId = "71520b38-69f6-4cc6-b8a3-e3ef859ed9a6";
-
   const router = useRouter();
-  console.log(router);
+  //const { shopId, noticeId } = router.query;
+
+  const shopId = router.query.shopId || "7f79ef71-553d-4c50-a35c-7cb48e978b4c";
+  const noticeId = router.query.noticeId || "71520b38-69f6-4cc6-b8a3-e3ef859ed9a6";
+
+  if (typeof shopId !== "string" || typeof noticeId !== "string") {
+    return <div>Loading</div>;
+  }
 
   return (
     <RootLayout>

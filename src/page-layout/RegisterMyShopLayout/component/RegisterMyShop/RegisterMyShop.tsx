688f1646573826da20ff4b3e38a5735805a13de8
minbo <smb5174@naver.com>
2024-04-29 14:25:42 +0900

    refactor: 무한 스크롤 수정 중

--- src/page-layout/RegisterMyShopLayout/component/RegisterMyShop/RegisterMyShop.tsx
+++ src/page-layout/RegisterMyShopLayout/component/RegisterMyShop/RegisterMyShop.tsx
@@ -14,6 +14,7 @@ import { usePostPresignedURL, usePostShopData } from "@/shared/apis/api-hooks";
 import { useRouter } from "next/router";
 import ConfirmModal from "@/common/components/Modal/ConfirmModal/ConfirmModal";
 import Image from "next/image";
+import useUserDataStore from "@/shared/hooks/useUserDataStore";
 
 interface ShopInfo {
   name: string;
@@ -42,7 +43,7 @@ export default function RegisterMyShopLayout() {
     resolver: zodResolver(schema),
     mode: "onTouched",
   });
-
+  const { setShopId } = useUserDataStore();
   const [shopName, setShopName] = useState("");
   const [classification, setClassification] = useState("");
   const [address, setAddress] = useState("");
@@ -52,7 +53,7 @@ export default function RegisterMyShopLayout() {
   const [description, setDescription] = useState("");
   const imgRef = useRef<HTMLInputElement>(null);
   const { data, mutate } = usePostPresignedURL();
-  const { mutate: shopDataMutate } = usePostShopData({
+  const { data: shopData, mutate: shopDataMutate } = usePostShopData({
     name: shopName,
     category: classification,
     address1: address,
@@ -67,6 +68,7 @@ export default function RegisterMyShopLayout() {
 
   const handleClickConfirm = async () => {
     await shopDataMutate();
+    setShopId(shopData); // shopId 저장해야함 -> 수정 필요
     router.push("/my-shop");
   };
 
--- src/pages/shops/index.tsx
+++ src/pages/shops/index.tsx
@@ -6,11 +6,8 @@ import { useInView } from "react-intersection-observer";
 import { useEffect, useState } from "react";
 
 import useGetMessages from "@/page-layout/MyShopLayout/hooks/useGetShopData";
-import { axiosInstance } from "@/shared/apis/axiosInstance";
-import { Item, ShopData } from "@/page-layout/MyShopLayout/type";
-import useUserDataStore from "@/shared/hooks/useUserDataStore";
+import { Item } from "@/page-layout/MyShopLayout/type";
 import GetUserData from "@/shared/hooks/getUserData";
-import { useGetUserData } from "@/shared/apis/api-hooks/useUsers";
 import { useGetShopData } from "@/shared/apis/api-hooks";
 
 interface DataItem {
@@ -19,22 +16,16 @@ interface DataItem {
 
 type DataArray = DataItem[];
 
-interface ApiData {
-  shopData: ShopData | null;
-}
-
 export default function MyShop() {
-  const { setShopIdFromData } = GetUserData();
-  setShopIdFromData();
-  const { shopId } = useUserDataStore();
+  const { shopId } = GetUserData();
   const { data: shopDataList } = useGetShopData(shopId);
-  console.log(shopDataList);
-  const filterShopData = shopDataList?.data.item;
+  const filterShopData = shopDataList?.item;
+
+  const [array, setArray] = useState<DataArray>([]);
 
   const [ref, inView] = useInView();
   const { data, fetchNextPage, hasNextPage } = useGetMessages();
   const registerdShopList = data?.pages;
-  const [array, setArray] = useState<DataArray>([]);
 
   if (registerdShopList !== undefined) {
     for (let i = 0; i < registerdShopList?.length; i += 1) {
@@ -49,14 +40,14 @@ export default function MyShop() {
   }
 
   useEffect(() => {
-    setArray((prev) => [...prev, ...(data?.pages[data.pages.length - 1].result ?? [])]);
-  }, [data]);
-
-  useEffect(() => {
     if (inView && hasNextPage) {
       fetchNextPage();
     }
   }, [inView, hasNextPage, fetchNextPage]);
+
+  useEffect(() => {
+    setArray((prev) => [...prev, ...(data?.pages[data.pages.length - 1].result ?? [])]);
+  }, [data]);
 
   return (
     <RootLayout>

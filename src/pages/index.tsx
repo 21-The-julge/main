6df4e49925846bea9cfd506e20cce50dd6038358
김보경 <rlaqhrud6263@naver.com>
2024-04-30 04:29:14 +0900

    feat: 메인페이지 메타태그 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from "next";
 import RootLayout from "@/shared/components/RootLayout/RootLayout";
 
 import Notices from "@/page-layout/NoticesLayout/components/Notices";
+
+export const metadata: Metadata = {
+  title: "The-julge",
+  description: "급하게 일손이 필요한 자리에 더 많은 시급을 제공해서 아르바이트생을 구할 수 있는 서비스입니다.",
+};
 
 export default function Home() {
   return (
--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames/bind";
 import Link from "next/link";
 
+import { Metadata } from "next";
 import { ROUTE } from "@/common/constants";
 import { SignUpForm } from "@/page-layout/AuthLayout/component";
 import ThejulgeLogo from "@/images/logo.svg";
@@ -8,6 +9,11 @@ import ThejulgeLogo from "@/images/logo.svg";
 import styles from "./SignUpPage.module.scss";
 
 const cn = classNames.bind(styles);
+
+export const metadata: Metadata = {
+  title: "The-julge",
+  description: "급하게 일손이 필요한 자리에 더 많은 시급을 제공해서 아르바이트생을 구할 수 있는 서비스입니다.",
+};
 
 export default function SignUpPage() {
   return (

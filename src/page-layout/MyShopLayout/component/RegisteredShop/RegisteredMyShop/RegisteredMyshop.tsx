1cb5d974dfcd85f582fb723fc485738660578674
KimChangmin <zzangmin3@gmail.com>
2024-04-29 22:36:07 +0900

    feat: 무한스크롤 기능 구현

--- src/page-layout/MyShopLayout/component/RegisteredShop/RegisteredMyShop/RegisteredMyshop.tsx
+++ src/page-layout/MyShopLayout/component/RegisteredShop/RegisteredMyShop/RegisteredMyshop.tsx
@@ -1,26 +1,51 @@
 import classNames from "classnames/bind";
 import styles from "@/page-layout/MyShopLayout/component/RegisteredShop/RegisteredMyShop/RegisteredMyShop.module.scss";
+import { ApiResponse, Item } from "@/shared/apis/apiType";
 
-import { RegisterdShop as RegistseredMyShopProps } from "@/page-layout/MyShopLayout/type";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
+import { useGetShopNoticesData } from "@/shared/apis/api-hooks/useNotices";
 
 import PostContainer from "./PostContainer/PostContainer";
+import Registser from "../Register/Register";
 
 const cn = classNames.bind(styles);
 
-export default function RegistseredMyShop({ lastRef, myShopData }: RegistseredMyShopProps) {
-  return (
+export default function RegistseredMyShop({ shopId }: { shopId: string }) {
+  const [lastRef, inView] = useInView();
+
+  const { shopNoticeData, hasNextPage, fetchNextPage } = useGetShopNoticesData({ shopId });
+
+  // console.log(shopNoticeData?.pages);
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
+  const RegisteredMyShopComponent = (
     <div className={cn("container")}>
       <div className={cn("registerContiner")}>
         <p className={cn("myRegisterdShop")}>내가 등록한 공고</p>
         <div className={cn("gridContainer")}>
-          {myShopData?.map((item, idx) => (
-            <div key={item.item.id}>
-              <PostContainer myShopData={item} />
-              <div ref={idx === myShopData.length - 1 ? lastRef : null} />
-            </div>
-          ))}
+          {shopNoticeData?.pages?.flatMap((page: ApiResponse, pageIndex) =>
+            page.items.map((item: Item, itemIndex) => (
+              <div key={item.item.id}>
+                <PostContainer myShopData={item} shopId={shopId} />
+                <div
+                  ref={
+                    pageIndex === shopNoticeData.pages.length - 1 && itemIndex === page.items.length - 1
+                      ? lastRef
+                      : null
+                  }
+                />
+              </div>
+            )),
+          )}
         </div>
       </div>
     </div>
   );
+  return shopNoticeData ? RegisteredMyShopComponent : <Registser />;
 }
--- src/pages/shops/index.tsx
+++ src/pages/shops/index.tsx
@@ -1,58 +1,19 @@
-import ShowMyShop from "@/page-layout/MyShopLayout/component/MyShop/MyShop";
-import RegisterdShop from "@/page-layout/MyShopLayout/component/RegisteredShop/RegisterdShop";
+import MyShop from "@/page-layout/MyShopLayout/component/MyShop/MyShop";
+import RegisterdMyShop from "@/page-layout/MyShopLayout/component/RegisteredShop/RegisteredMyShop/RegisteredMyshop";
 import RootLayout from "@/shared/components/RootLayout/RootLayout";
 
-import { useInView } from "react-intersection-observer";
-import { useEffect, useState } from "react";
-
-import useGetMessages from "@/page-layout/MyShopLayout/hooks/useGetShopData";
-import { Item } from "@/page-layout/MyShopLayout/type";
 import GetUserData from "@/shared/hooks/getUserData";
 import { useGetShopData } from "@/shared/apis/api-hooks";
 
-interface DataItem {
-  item: Item;
-}
-
-type DataArray = DataItem[];
-
-export default function MyShop() {
+export default function MyShopPage() {
   const { shopId } = GetUserData();
   const { data: shopDataList } = useGetShopData(shopId);
   const filterShopData = shopDataList?.item;
 
-  const [array, setArray] = useState<DataArray>([]);
-
-  const [ref, inView] = useInView();
-  const { data, fetchNextPage, hasNextPage } = useGetMessages();
-  const registerdShopList = data?.pages;
-
-  if (registerdShopList !== undefined) {
-    for (let i = 0; i < registerdShopList?.length; i += 1) {
-      // eslint-disable-next-line no-restricted-syntax
-      for (const j of registerdShopList[i].result) {
-        j.item.imageUrl = filterShopData?.imageUrl;
-        j.item.name = filterShopData?.name;
-        j.item.address1 = filterShopData?.address1;
-        j.item.originalHourlyPay = filterShopData?.originalHourlyPay;
-      }
-    }
-  }
-
-  useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage();
-    }
-  }, [inView, hasNextPage, fetchNextPage]);
-
-  useEffect(() => {
-    setArray((prev) => [...prev, ...(data?.pages[data.pages.length - 1].result ?? [])]);
-  }, [data]);
-
   return (
     <RootLayout>
-      <ShowMyShop myShopData={filterShopData} />
-      {filterShopData ? <RegisterdShop lastRef={ref} myShopData={array} /> : null}
+      <MyShop myShopData={filterShopData} />
+      {filterShopData ? <RegisterdMyShop shopId={shopId} /> : null}
     </RootLayout>
   );
 }
